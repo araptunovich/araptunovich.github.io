07342de44720aa7abcc1bb1da05375518659db09
Anna <araptunovich@gmail.com>
2018-12-13 22:26:07 -0500

    pew

---
+++ contact.js
@@ -0,0 +1,20 @@
+<div id="wufoo-z1qkdla803kd1rb">
+Fill out my <a href="https://excalisoft.wufoo.com/forms/z1qkdla803kd1rb">online form</a>.
+</div>
+<div id="wuf-adv" style="font-family:inherit;font-size: small;color:#a7a7a7;text-align:center;display:block;">HTML Forms powered by <a href="http://www.wufoo.com">Wufoo</a>.</div>
+<script type="text/javascript">var z1qkdla803kd1rb;(function(d, t) {
+var s = d.createElement(t), options = {
+'userName':'excalisoft',
+'formHash':'z1qkdla803kd1rb',
+'autoResize':true,
+'height':'557',
+'async':true,
+'host':'wufoo.com',
+'header':'show',
+'ssl':true};
+s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+s.onload = s.onreadystatechange = function() {
+var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+try { z1qkdla803kd1rb = new WufooForm();z1qkdla803kd1rb.initialize(options);z1qkdla803kd1rb.display(); } catch (e) {}};
+var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+})(document, 'script');</script>
